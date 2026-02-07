141156e2b6a036214991ccac85783d2eeef2eaa8
CodeWith-Het <hetp1065@gmail.com>
2026-02-07 10:49:14 -0600

    aura day 5 ka

---
+++ Day-05/server.js
@@ -0,0 +1,40 @@
+require("dotenv").config()
+
+const app = require('./src/app')
+const mongoose = require("mongoose")
+const user = require('./src/models/user')
+
+
+
+function connectToDB(){
+  return mongoose.connect(process.env.MONGO_URI)
+
+  .then(()=>{
+
+    console.log("Database Connected")
+
+    return user.create([
+        {name:"het",surname:"patel"},
+        {name:"vishv",surname:"patel"},
+        {name:"nidhi",surname:"jariwala"}
+    ])
+  })
+
+    .then(()=>{
+        console.log("Data inserted")
+    })
+
+    .catch((err)=>{
+        console.log("error not connected",err)
+    })
+}
+
+connectToDB()
+
+app.get("/" ,(req,res)=>{
+    res.send("hello day 5 ka server kaise ho")
+})
+
+app.listen(3000,()=>{
+    console.log("server started at port 3000")
+})
